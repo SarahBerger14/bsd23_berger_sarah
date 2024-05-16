f97d1cf57df2714b322db2d219311be48dcff2e5
Sarah Berger <s52969@edu.campus02.at>
2024-05-16 22:25:24 +0200

    Loggin branch was made and loggin object with level info and error was added in Main Class

--- src/main/java/at/campus02/bsd/Main.java
+++ src/main/java/at/campus02/bsd/Main.java
@@ -1,6 +1,11 @@
 package at.campus02.bsd;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
+    private static final Logger logger = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
 
         System.out.println("Sarah Berger");
@@ -16,5 +21,8 @@ public class Main {
 
         double resultDivide = Calculator.divide(12.5, 45.3);
         System.out.println("Division: " + resultDivide);
+
+        logger.info("Hello, this is an info message");
+        logger.error("Hello, this is an error message");
     }
 }
