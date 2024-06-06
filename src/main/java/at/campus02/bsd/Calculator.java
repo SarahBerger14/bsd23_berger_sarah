5522d5953767efd302ec071566ccae9f890690cf
Sarah Berger <s52969@edu.campus02.at>
2024-06-06 10:25:12 +0200

    Added javadoc to the Calculator class

--- src/main/java/at/campus02/bsd/Calculator.java
+++ src/main/java/at/campus02/bsd/Calculator.java
@@ -1,20 +1,48 @@
 package at.campus02.bsd;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+/**
+ * This class provides basic arithmetic operations.
+ * The operations are added, minus, divide, multiply and factorial.
+ * The class also provides logging for each operation.
+ */
 public class Calculator {
 
+    /**
+     * The logger is used to log the operations.
+     */
     private static final Logger logger = LogManager.getLogger(Calculator.class);
 
+    /**
+     * This method adds two numbers.
+     * @param number1 The first number to add.
+     * @param number2 The second number to add.
+     * @return The sum of the two numbers.
+     */
     public static double add(double number1, double number2) {
         logger.debug("{} + {}", number1, number2);
         return number1 + number2;
     }
 
+    /**
+     * This method is for subtraction with two numbers.
+     * @param number1 minuet.
+     * @param number2 subtrahend.
+     * @return The difference of the two numbers.
+     */
     public static double minus(double number1, double number2) {
         logger.debug("{} - {}", number1, number2);
         return number1 - number2;
     }
 
+    /**
+     * This method divides two numbers.
+     * @param number1 The dividend.
+     * @param number2 The divisor.
+     * @throws ArithmeticException If the divisor is zero.
+     * @return The result of the division.
+     */
     public static double divide(double number1, double number2) {
         logger.debug("{} / {}", number1, number2);
         if(number2 == 0) {
@@ -24,11 +52,22 @@ public class Calculator {
         return number1 / number2;
     }
 
+    /**
+     * This method multiplies two numbers.
+     * @param number1 The first number to multiply.
+     * @param number2 The second number to multiply.
+     * @return The product of the two numbers.
+     */
     public static double multiply(double number1, double number2) {
         logger.debug("{} * {}", number1, number2);
         return number1 * number2;
     }
 
+    /**
+     * This method calculates the factorial of a number. If the number is negative, the result is 0.
+     * @param factorial The number for which the factorial is calculated.
+     * @return The factorial of the number.
+     */
     public static double factorial(double factorial) {
         logger.debug("Factorial of {}", factorial);
         double result = 1;
